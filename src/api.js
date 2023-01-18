a844a318175ff4f19ec94fb3389c6872c61aa739
Sahil <sahilgehani47@gmail.com>
2023-01-18 17:15:00 +0530

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+	  "X-RapidAPI-Key": "b9dca43723msh716d407a2197d79p1b8d21jsn59fb48bc5234",// enter your rapid api key here
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "576874068f369341db9c0e9bf9cd5762"; // enter your key from openweather API
